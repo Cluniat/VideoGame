92bafbd653117fe5bed84e33ac91ffc0aadf8dce
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2018-07-15 08:24:33 +0200

    starting calling API with axios

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,12 +8,36 @@
 <script>
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
+import axios from "axios";
+
 export default {
   name: 'Home',
     components: {SearchBar, Header},
     props: {
     msg: String
-  }
+  },
+    created: function() {
+
+
+        axios.get("https://api.raidhead.com/fra/games/index.json", {
+            headers: {
+                "user-key": "737bc70227de8d102078bcc22c8992a7",
+                Accept: "application/json",
+                // 'Access-Control-Allow-Origin': '*',
+                // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+            },
+        })
+        .then(response => {
+            // Do work here
+            alert(response.data);
+        })
+        .catch(e => {
+            /* eslint-disable */
+            console.log(e)
+           // alert(e);
+        });
+
+    }
 }
 </script>
 
